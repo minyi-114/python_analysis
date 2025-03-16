a5b2d426039fe918c700dab9592106246b0c78a6
minyi-114 <minyis@gmail.com>
2025-03-16 11:23:53 +0800

    aa

--- lesson8/lesson8_3.py
+++ lesson8/lesson8_3.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def main():
+def main():  #自訂主程式
         df = pd.read_csv('上市公司資料.csv')
         df1 = df.dropna()
         df2 = df1.reindex(columns=['公司代號','出表日期','公司名稱','產業別','營業收入-當月營收','營業收入-上月營收'])
@@ -12,6 +12,6 @@ def main():
         df3.to_excel('上市公司資料整理.xlsx')
         print("存檔完成")
 
-if __name__ == '__main__':
+if __name__ == '__main__': #呼叫主程式
         main()
 
